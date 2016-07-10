6967cb53bc4f0bb6049cd84243ac126a844f56a3
Kim Curran <currankim7@gmail.com>
2016-07-09 20:25:14 -0700

    Add working server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,20 @@
-//index.js
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+var port = process.env.PORT || 8000;
+
+app.use(express.static(__dirname + '/../client'));
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(bodyParser.json());
+
+app.listen(port, function(err) {
+  if(err){
+    return console.log(err);
+  } else {
+    console.log('Listening on Port: ' + port);
+  }
+});
