44dc708a1f67e766fb5d96bdb9a156f2f2cb5f9b
Kim Curran <currankim7@gmail.com>
2016-09-11 20:33:44 -0700

    Remove extraneous server code

--- server/index.js
+++ server/index.js
@@ -1,15 +1,10 @@
 var express = require('express');
-// var bodyParser = require('body-parser');
 
 var app = express();
 
 var port = process.env.PORT || 8000;
 
 app.use(express.static(__dirname + '/../client'));
-
-// app.use(bodyParser.urlencoded({extended:true}));
-
-// app.use(bodyParser.json());
 
 app.listen(port, function(err) {
   if(err){
